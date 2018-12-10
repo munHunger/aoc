d557cf9f29137198c0a03d63dde6715d7fb35e0f
munHunger <marcusmunger@hotmail.com>
2018-12-10 20:28:05 +0100

    solved part 2

--- 2018/05/solve.js
+++ 2018/05/solve.js
@@ -12,17 +12,31 @@ function generateRegex() {
   );
 }
 
-function part1(input) {
-  input = input[0];
-  let regex = generateRegex();
+function shorten(input, regex) {
   let next = input;
   do {
     input = next;
     next = input.replace(regex, "");
   } while (next.length < input.length);
-  return input.length;
+  return input;
 }
 
-function part2(input) {}
+function part1(input) {
+  input = input[0];
+  let regex = generateRegex();
+  return shorten(input, regex).length;
+}
+
+function part2(input) {
+  input = input[0];
+  let regex = generateRegex();
+  return "abcdefghijklmnopqrstuvwxyz"
+    .split("")
+    .map(c => c + "|" + c.toUpperCase())
+    .map(s => new RegExp(s))
+    .map(regex => shorten(input, regex))
+    .map(red => shorten(red, regex))
+    .sort((a, b) => a.length - b.length)[0].length;
+}
 
 module.exports = { solve };
--- 2018/05/solve.spec.js
+++ 2018/05/solve.spec.js
@@ -3,3 +3,6 @@ const { solve } = require("./solve");
 describe("Part 1", () => {
   it("solves for 10", () => expect(solve(["dabAcCaCBAcCcaDA"], 1)).toBe(10));
 });
+describe("Part 2", () => {
+  it("solves for 4", () => expect(solve(["dabAcCaCBAcCcaDA"], 2)).toBe(4));
+});
