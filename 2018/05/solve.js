bfe5d636624af166507a1d9d24bc4dddf78b8691
munHunger <marcusmunger@hotmail.com>
2018-12-10 20:17:08 +0100

    solved part 1

--- 2018/05/solve.js
+++ 2018/05/solve.js
@@ -3,7 +3,25 @@ function solve(input, part) {
   else return part2(input);
 }
 
-function part1(input) {}
+function generateRegex() {
+  return new RegExp(
+    "abcdefghijklmnopqrstuvwxyz"
+      .split("")
+      .map(c => c + c.toUpperCase() + "|" + c.toUpperCase() + c)
+      .join("|")
+  );
+}
+
+function part1(input) {
+  input = input[0];
+  let regex = generateRegex();
+  let next = input;
+  do {
+    input = next;
+    next = input.replace(regex, "");
+  } while (next.length < input.length);
+  return input.length;
+}
 
 function part2(input) {}
 
