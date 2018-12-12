dedde4c8a8033f690d99a52536f513c87a0d931a
Marcus Münger <marcus.munger@cybercom.com>
2018-12-12 16:48:17 +0100

    Added day 7

---
+++ 2018/07/solve.spec.js
@@ -0,0 +1,19 @@
+const { solve } = require("./solve");
+
+describe("Part 1", () => {
+  it("solves for CABDFE", () =>
+    expect(
+      solve(
+        [
+          "Step C must be finished before step A can begin.",
+          "Step C must be finished before step F can begin.",
+          "Step A must be finished before step B can begin.",
+          "Step A must be finished before step D can begin.",
+          "Step B must be finished before step E can begin.",
+          "Step D must be finished before step E can begin.",
+          "Step F must be finished before step E can begin."
+        ],
+        1
+      )
+    ).toBe("CABDFE"));
+});
