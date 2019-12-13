17b46fa74ebd30b105d444e9c8848280277151d5
munHunger <marcusmunger@hotmail.com>
2019-12-13 07:51:09 +0100

    wip

--- 2019/05/solve.js
+++ 2019/05/solve.js
@@ -5,19 +5,22 @@ function solve(input, part) {
 
 function part1(input) {
   input = input[0].split(",").map(n => parseInt(n));
-  return computer(input);
+  return computer(input, 1);
 }
 
-function part2(input) {}
+function part2(input) {
+  input = input[0].split(",").map(n => parseInt(n));
+  return computer(input, 5);
+}
 
-function computer(input) {
+function computer(input, start) {
   let index = 0;
   let stack = [];
   while (input[index] != 99) {
     let [op, a, b, c] = input.slice(index, index + 4);
-    a = { ref: Math.floor(op / 100) === 0 ? input[a] : a, raw: a };
-    b = { ref: Math.floor(op / 1000) === 0 ? input[b] : b, raw: b };
-    c = { ref: Math.floor(op / 10000) === 0 ? input[c] : c, raw: c };
+    a = { ref: Math.floor((op % 1000) / 100) === 0 ? input[a] : a, raw: a };
+    b = { ref: Math.floor((op % 10000) / 1000) === 0 ? input[b] : b, raw: b };
+    c = { ref: Math.floor((op % 100000) / 10000) === 0 ? input[c] : c, raw: c };
     let frame = { op, a, b, c, index };
     stack.push(frame);
     let r;
@@ -29,14 +32,32 @@ function computer(input) {
         r = { val: a.ref * b.ref, consumed: 4, out: c.raw };
         break;
       case 3: //Input
-        r = { val: 1, consumed: 2, out: a.raw };
+        r = { val: start, consumed: 2, out: a.raw };
         break;
       case 4: //Output
-        if (a.ref !== 0) console.log("shit");
         console.log(a.ref);
         r = { consumed: 2 };
         break;
+      case 5:
+        if (a !== 0) {
+          index = b.ref;
+          r = { consumed: 0 };
+        }
+        break;
+      case 6:
+        if (a === 0) {
+          index = b.ref;
+          r = { consumed: 0 };
+        }
+        break;
+      case 7:
+        r = { consumed: 4, out: c.raw, val: a.ref < b.ref ? 1 : 0 };
+        break;
+      case 8:
+        r = { consumed: 4, out: c.raw, val: a.ref === b.ref ? 1 : 0 };
+        break;
     }
+    console.log(frame);
     if (op % 10 != 4) {
       input[r.out] = r.val;
       frame.val = r.val;
