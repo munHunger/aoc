2626feee2ddc29bf0c01a1147b570d3283fd591b
munHunger <marcusmunger@hotmail.com>
2025-12-05 14:08:24 +0000

    part2

--- 2025/01/solve.js
+++ 2025/01/solve.js
@@ -1,5 +1,6 @@
 function solve(input, part) {
   if (part === 1) return part1(input);
+  return part2(input);
 }
 
 function part1(input) {
@@ -20,6 +21,32 @@ function part1(input) {
         if(start == 0) {
             count++;
         }
+    }
+
+    return count
+}
+
+
+function part2(input) {
+    let pos = 50;
+    let count = 0;
+    for (let row of input) {
+        let value = parseInt(row.substring(1));
+
+        let distanceToZero = pos === 0 ? 100 : (row.startsWith("L") ? pos : 100 - pos)
+        if(value >= distanceToZero) {
+            let change = 1 + Math.floor((value - distanceToZero) / 100);
+            count += change
+        }
+
+        if(row.startsWith("L")) {
+            pos -= value;
+        }
+        else {
+            pos += value;
+        }
+        pos = ((pos % 100) + 100) % 100;
+        
     }
 
     return count
